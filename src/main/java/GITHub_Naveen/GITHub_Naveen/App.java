af0c75b7d2ca23edeac536e48dd6777fb2ff9f4d
Shivsandesh Shete <sandesh.shete2015@gmail.com>
2019-07-19 15:22:28 +0530

    First GIT commit for my selenium code

---
+++ src/main/java/GITHub_Naveen/GITHub_Naveen/App.java
@@ -0,0 +1,13 @@
+package GITHub_Naveen.GITHub_Naveen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
